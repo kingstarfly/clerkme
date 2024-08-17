9e3b53da4f72ddd5b710242d72ee9a74029362d3
kingstarfly <xingxiang@twotreesgroup.com>
2024-08-18 01:02:23 +0800

    create form

---
+++ src/app/_components/NewCaseRecordForm/FormTextArea.tsx
@@ -0,0 +1,40 @@
+import {
+  type FieldError,
+  type FieldValues,
+  type Path,
+  type UseFormRegister,
+} from "react-hook-form";
+
+interface FormTextAreaProps<T extends FieldValues>
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+  id: Path<T>;
+  label: string;
+  register: UseFormRegister<T>;
+  error?: FieldError;
+  rows?: number;
+}
+
+export function FormTextArea<T extends FieldValues>({
+  id,
+  label,
+  register,
+  error,
+  rows = 3,
+}: FormTextAreaProps<T>) {
+  return (
+    <div>
+      <label htmlFor={id} className="block text-sm font-medium text-gray-700">
+        {label}
+      </label>
+      <textarea
+        id={id}
+        rows={rows}
+        {...register(id)}
+        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+      />
+      {error?.message !== undefined && (
+        <p className="mt-1 text-sm text-red-600">{error.message}</p>
+      )}
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import { api, HydrateClient } from "@/trpc/server";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import NewCaseRecordForm from "./_components/NewCaseRecordForm/NewCaseRecordForm";
 
 export default async function Home() {
   const allCaseRecords = await api.caseRecord.getAll();
 
   return (
     <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <main className="flex min-h-screen flex-col items-center justify-center">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
           <SignedOut>
             <SignInButton />
@@ -18,6 +19,8 @@ export default async function Home() {
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
             Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
           </h1>
+
+          <NewCaseRecordForm />
 
           <div className="flex flex-col items-center gap-2">
             <p className="text-2xl text-white">
